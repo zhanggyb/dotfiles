de7ac391b9c2a08a6dd0103fb10217f64389a08b
Freeman Zhang <zhanggyb@localhost.localdomain>
2013-03-26 22:30:46 +0800

    add my favorite font config

--- install.sh
+++ install.sh
@@ -3,10 +3,11 @@
 # Install the dot files to the HOME directory
 
 DIST=$HOME/.config/shell
+FONT_CONFIG_DIR=$HOME/.config/fontconfig
 
 if [ ! -d $DIST ];
 then
-	mkdir -p $DIST || (echo "Fail to create folder! Abort."; exit 1)
+	mkdir -p $DIST || (echo "Fail to create $DIST! Abort."; exit 1)
 fi
 
 cp -f bashrc ~/.bashrc
@@ -16,4 +17,11 @@ cp -f functions $DIST
 
 cp -f Xresources ~/.Xresources
 xrdb ~/.Xresources
+
+if [ ! -d $FONT_CONFIG_DIR ];
+then
+	mkdir -p $FONT_CONFIG_DIR || (echo "Fail to create $FONT_CONFIG_DIR! Abort."; exit 1)
+fi
+
+cp -f fonts.conf $FONT_CONFIG_DIR
 
