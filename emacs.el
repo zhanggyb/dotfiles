04deb2f2ca6c4db729529956c554380005200105
Freeman Zhang <zhanggyb@gmail.com>
2013-08-01 09:47:49 +0800

    add emacs setting remove JAVA env for netbeans

---
+++ emacs.el
@@ -0,0 +1,9 @@
+;; enable menu bar
+(menu-bar-mode t)
+
+(setq-default c-basic-offset 4
+              tab-width 4
+              indent-tabs-mode t)
+;(setq c-default-style "stroustrup" c-basic-offset 4)
+(c-set-offset 'substatement-open 0)
+
