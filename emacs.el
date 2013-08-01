eeb15c9a17cd8b877db75a74e5ff1e88699261e3
Freeman Zhang <zhanggyb@gmail.com>
2013-08-01 13:32:05 +0800

    add to c-mode hook

--- emacs.el
+++ emacs.el
@@ -1,9 +1,21 @@
 ;; enable menu bar
 (menu-bar-mode t)
 
-(setq-default c-basic-offset 4
-              tab-width 4
-              indent-tabs-mode t)
-;(setq c-default-style "stroustrup" c-basic-offset 4)
-(c-set-offset 'substatement-open 0)
+(setq prelude-whitespace nil)
+
+(defun my-c-mode-common-hook ()
+  ;; my customizations for all of c-mode, c++-mode, objc-mode, java-mode
+  (c-set-offset 'substatement-open 0)
+  ;; other customizations can go here
+
+  (setq c++-tab-always-indent t)
+  (setq c-basic-offset 4)                  ;; Default is 2
+  (setq c-indent-level 4)                  ;; Default is 2
+
+  (setq tab-stop-list '(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60))
+  (setq tab-width 4)
+  (setq indent-tabs-mode t)  ; use spaces only if nil
+  )
+
+(add-hook 'c-mode-common-hook 'my-c-mode-common-hook)
 
